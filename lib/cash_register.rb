3ef79ed038b44140437812124c77db7ea80283a7
Hunter Wood <hunter674530@gmail.com>
2023-01-10 12:22:36 -0600

    commit tree 468a38d43bbcf576f90471bd93cc9b423ca6102a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+require 'pry'
 
+class CashRegister
+
+  attr_reader : discount, total
+  attr_writer : discount, total
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
+
+end
